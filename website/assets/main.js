a417d561b688b7c954d12922738ed2a477abe18e
Josh Teng <josh@local.com>
2021-05-12 20:52:30 +0800

    super ugly UI

--- website/assets/main.js
+++ website/assets/main.js
@@ -293,12 +293,66 @@ function getBankBalance() {
   })
 }
 
-window.addEventListener('load', async () => {
+function getUserBankBalance() {
+  const bankContract = new web3.eth.Contract(bankAbi, "0x89670662006E21bcBe5e926e1f5D6917c2225356")
+
+  bankContract.methods.balanceOf(window.ethereum.selectedAddress).call().then((balance) => {
+    document.getElementById('user-bank-balance').innerHTML = window.web3.utils.fromWei(balance);
+  })
+}
+
+async function deposit(amount) {
+  const bankContract = new window.web3.eth.Contract(bankAbi, "0x89670662006E21bcBe5e926e1f5D6917c2225356")
+
+  const account = window.ethereum.selectedAddress
+
+  console.log(account)
+
+  amount = new window.web3.utils.BN(web3.utils.toWei(amount.toString()))
+  console.log(await bankContract.methods.deposit().send({
+    from: account,
+    value: new window.web3.utils.BN(amount.toString())
+  }))
+}
+
+async function transfer(address, amount) {
+  const bankContract = new window.web3.eth.Contract(bankAbi, "0x89670662006E21bcBe5e926e1f5D6917c2225356")
+
+  const account = window.ethereum.selectedAddress
+
+  console.log(account)
+
+  amount = new window.web3.utils.BN(amount.toString())
+  console.log(await bankContract.methods.transfer(address, amount).send({
+    from: account
+  }))
+}
+
+
+
+async function send(address, amount) {
+  const bankContract = new window.web3.eth.Contract(bankAbi, "0x89670662006E21bcBe5e926e1f5D6917c2225356")
+
+  const account = window.ethereum.selectedAddress
+
+  console.log(account)
+
+  amount = new window.web3.utils.BN(web3.utils.toWei(amount.toString()))
+  console.log(await bankContract.methods.send(address, amount).send({
+    from: account
+  }))
+}
+
+
+const connectButton = document.getElementById('connect')
+
+connectButton.addEventListener('click', async (event) => {
   // Modern dapp browsers...
   if (window.ethereum) {
     window.web3 = new Web3(ethereum);
     try {
-      await ethereum.sendAsync('eth_requestAccounts');
+      await ethereum.request({ method: 'eth_requestAccounts' });
+      getUserBankBalance()
     } catch (error) {
       // User denied account access...
     }
@@ -306,18 +360,56 @@ window.addEventListener('load', async () => {
   // Legacy dapp browsers...
   else if (window.web3) {
     window.web3 = new Web3(web3.currentProvider);
+    getUserBankBalance()
   }
   // Non-dapp browsers...
   else {
     console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
   }
+
+  event.preventDefault();
+
 });
+
 
 
 document.getElementById('get-bank-balance').addEventListener('click', function (event) {
 
   getBankBalance()
 
+  // Don't follow the link
+  event.preventDefault();
+
+}, false);
+
+const transferForm = document.getElementById('transfer')
+
+transferForm.addEventListener('submit', function (event) {
+  const address = transferForm.querySelector('input[name="address"]').value
+  const amount = transferForm.querySelector('input[name="amount"]').value
+  transfer(address, amount)
+  // Don't follow the link
+  event.preventDefault();
+
+}, false);
+
+const sendForm = document.getElementById('send-balance')
+
+sendForm.addEventListener('submit', function (event) {
+  const address = sendForm.querySelector('input[name="address"]').value
+  const amount = sendForm.querySelector('input[name="amount"]').value
+  send(address, amount)
+  // Don't follow the link
+  event.preventDefault();
+
+}, false);
+
+
+const depositForm = document.getElementById('deposit')
+
+depositForm.addEventListener('submit', function (event) {
+  const amount = depositForm.querySelector('input[name="amount"]').value
+  deposit(amount)
   // Don't follow the link
   event.preventDefault();
 
